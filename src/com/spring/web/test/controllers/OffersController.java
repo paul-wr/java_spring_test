92327423f99adba98be0700aeadbcf25b4880824
paul <mainaccount@Jupitors-iMac.local>
2017-09-28 13:38:19 +0100

    first commit

---
+++ src/com/spring/web/test/controllers/OffersController.java
@@ -0,0 +1,56 @@
+package com.spring.web.test.controllers;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.spring.web.test.dao.Offer;
+import com.spring.web.test.service.OffersService;
+
+@Controller
+public class OffersController {
+	
+	private OffersService offersService; 
+
+	
+	/*@RequestMapping("/")
+	public ModelAndView showHome(HttpSession session){
+		
+		session.setAttribute("name", "Robert");
+		
+		ModelAndView mv = new ModelAndView("home");
+		
+		Map<String, Object> model = mv.getModel();
+		
+		model.put("modelName", "Joan");
+		
+		return mv;
+	}*/
+	
+	@Autowired
+	public void setOffersService(OffersService offersService) {
+		this.offersService = offersService;
+	}
+
+	@RequestMapping("/offers")
+	public String showOffers(Model model){
+		
+		List<Offer> offers = offersService.getCurrent();
+		
+		model.addAttribute("offers", offers);
+		
+		return "offers";
+	}
+	
+	@RequestMapping("/createoffer")
+	public String createOffer(){
+		
+		return "createoffer";
+	}
+	
+}
