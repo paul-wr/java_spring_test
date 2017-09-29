9feac379a87cb89641a02dfb241229e615f703ae
paul <mainaccount@Jupitors-iMac.local>
2017-09-29 12:55:41 +0100

    Form updated

--- src/com/spring/web/test/dao/Offer.java
+++ src/com/spring/web/test/dao/Offer.java
@@ -4,14 +4,17 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import com.spring.web.test.validation.ValidEmail;
+
 public class Offer {
 	private int id;
 	
 	@Size(min = 5, max = 100, message="Name must be between 5 and 100 characters")
 	private String name;
 	
-	@Size(min = 5, max = 100)
-	@Pattern(regexp = ".*\\@.*\\..*", message="This does not appear to be a valid email address.")
+	@NotNull
+	// @Pattern(regexp = ".*\\@.*\\..*", message="This does not appear to be a valid email address.")
+	@ValidEmail(min = 6, message="This email address is not valid.")
 	private String email;
 
 	@Size(min = 20, max = 255, message="Text must be between 20 and 255 characters")
